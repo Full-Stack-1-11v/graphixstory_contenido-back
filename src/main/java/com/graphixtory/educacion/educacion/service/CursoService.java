9d6238d531e3b52609025e55a35fcc2bb43df77e
Vania <Van.carvajal@duocuc.cl>
2025-05-20 17:56:56 -0400

    Avance día Martes

--- src/main/java/com/graphixtory/educacion/educacion/service/CursoService.java
+++ src/main/java/com/graphixtory/educacion/educacion/service/CursoService.java
@@ -1,4 +1,33 @@
 package com.graphixtory.educacion.educacion.service;
 
+import com.graphixtory.educacion.educacion.model.Curso;
+import com.graphixtory.educacion.educacion.repository.CursoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class CursoService {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> findAll(){
+        return cursoRepository.findAll();
+    }
+
+    public Curso findById(long id){
+        return cursoRepository.findById(id).get();
+    }
+
+    public Curso save(Curso curso){
+        return cursoRepository.save(curso);
+    }
+
+    public void delete(long id){
+        cursoRepository.deleteById(id);
+    }
 }
