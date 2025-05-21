55158494d805c9c2e21ed99fb5f41aeac564a23f
Vina del Mar <vina@duoc.acad>
2025-05-20 21:52:18 -0400

    trabajo del martes durante clases

--- src/main/java/com/graphixtory/educacion/educacion/controller/CursoController.java
+++ src/main/java/com/graphixtory/educacion/educacion/controller/CursoController.java
@@ -1,4 +1,30 @@
 package com.graphixtory.educacion.educacion.controller;
 
+import com.graphixtory.educacion.educacion.model.Curso;
+import com.graphixtory.educacion.educacion.service.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/curso")
 public class CursoController {
+
+    @Autowired
+
+    private CursoService cursoService;
+
+    @GetMapping
+    public ResponseEntity<List<Curso>> listar(){
+
+        List<Curso> curso = cursoService.findAll();
+        if (curso.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(curso);
+    }
 }
