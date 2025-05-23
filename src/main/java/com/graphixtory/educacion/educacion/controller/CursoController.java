d671320912068647168b27a13dd6a4625198a86c
Vania <Van.carvajal@duocuc.cl>
2025-05-23 02:03:23 -0400

    Curso Controlador

--- src/main/java/com/graphixtory/educacion/educacion/controller/CursoController.java
+++ src/main/java/com/graphixtory/educacion/educacion/controller/CursoController.java
@@ -2,7 +2,6 @@ package com.graphixtory.educacion.educacion.controller;
 
 import com.graphixtory.educacion.educacion.model.Curso;
 import com.graphixtory.educacion.educacion.service.CursoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +12,35 @@ import java.util.List;
 @RequestMapping
 public class CursoController {
 
-    @Autowired
-    private CursoService cursoService;
+    private final CursoService cursoService;
+
+
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Curso>> getAllCursos(){
-
+    public ResponseEntity<List<Curso>> listarCursos() {
         List<Curso> cursos = cursoService.findAll();
-        if(cursos.isEmpty()){
+        if (cursos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(cursos);
     }
 
     @PostMapping
-    public ResponseEntity<Curso> saveCurso(@RequestBody Curso curso){
+    public ResponseEntity<Curso> guardarCursos(@RequestBody Curso curso) {
         Curso savedCurso = cursoService.save(curso);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCurso);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Curso> getCursoById(@PathVariable Long id){
-        try{
+    public ResponseEntity<Curso> buscarCursos(@PathVariable Integer id) {
+        try {
             Curso curso = cursoService.findById(id);
             return ResponseEntity.ok(curso);
         } catch (Exception e) {
+
             return ResponseEntity.notFound().build();
         }
     }
