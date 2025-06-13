60388afb407b1e92fc0d0ac7fc7e727da0a524bb
Vania <Van.carvajal@duocuc.cl>
2025-06-13 00:49:42 -0400

    Última Actualización

--- src/test/java/com/graphixtory/educacion/educacion/repository/ContenidoRepositoryTest.java
+++ src/test/java/com/graphixtory/educacion/educacion/repository/ContenidoRepositoryTest.java
@@ -1,5 +1,10 @@
 package com.graphixtory.educacion.educacion.repository;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ContenidoRepositoryTest {
+
+
 
 }
